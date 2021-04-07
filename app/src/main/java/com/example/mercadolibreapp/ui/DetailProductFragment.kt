10da2b1d9d921c02a52f3d16a6d9e0a825f88c4c
andresarangopro <andresarangopro@gmail.com>
2021-04-07 10:30:38 -0500

    update

--- app/src/main/java/com/example/mercadolibreapp/ui/DetailProductFragment.kt
+++ app/src/main/java/com/example/mercadolibreapp/ui/DetailProductFragment.kt
@@ -35,7 +35,7 @@ class DetailProductFragment : Fragment() {
         getViewModel {
             productDetailComponent.detailProductViewModel
         }
-    } 
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
